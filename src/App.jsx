82d4589d322a7865613425adedeac14a66bae386
DESKTOP-BHI57B9\USER-PC <yamilet.jerez@dyntra.org>
2023-05-04 09:10:33 +0200

    prueba de git

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+    useEffect (() => {
+    fetch("https://jsonplaceholder.typicode.com/users").then((res) => {
+      console.log(res);
+    });
+    }, []);
+
+  return <>
+  <h1>UseEffect</h1>
+  <button onClick={() => setCounter(counter +1)}>Counter : {counter}</button>
+  </>;
+};
+
+export default App;

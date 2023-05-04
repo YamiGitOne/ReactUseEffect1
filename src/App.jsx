d877c8d4d092fd74b2e196268159c42cdbd02913
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-04 09:18:36 +0200

    prueba de usuario correcto

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-    useEffect (() => {
+   /*  useEffect (() => {
     fetch("https://jsonplaceholder.typicode.com/users").then((res) => {
       console.log(res);
     });
-    }, []);
+    }, []); */
 
   return <>
   <h1>UseEffect</h1>

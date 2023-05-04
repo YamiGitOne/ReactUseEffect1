dd3faaa3405827693a929e19629e2f02b36a1c5b
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-04 14:35:00 +0200

    -

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0);
   const [users, setUsers] = useState(null);
-  
+
   console.log("App");
+    async function fetchData(){
+    const res = await fetch (
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    const data = await res.json();
+    setUsers(data);
+  }
+
     useEffect (() => {
       console.log("useEffect");
-      async function fetchData(){
-        const res = await fetch (
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        const data = await res.json();
-        setUsers(data);
-      }
       fetchData();
     }, []);
 
